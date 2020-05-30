0d2aa2eafb41db171ac99ca4885d284dab239c9c
Avaer Kazmer <a@modules.io>
2020-05-30 04:28:05 -0400

    Add volume capture command support

--- cli.js
+++ cli.js
@@ -305,6 +305,96 @@ const _screenshotApp = async output => {
   const buffer = builder.createBundle();
   fs.writeFileSync(output, buffer);
 };
+const _volumeApp = async output => {
+  const app = express();
+  app.use((req, res, next) => {
+    res.set('Access-Control-Allow-Origin', '*');
+    res.set('Access-Control-Allow-Methods', '*');
+    res.set('Access-Control-Allow-Headers', '*');
+    next();
+  });
+  app.get('/a.wbn', (req, res) => {
+    fs.createReadStream(output).pipe(res);
+  });
+  const _readIntoPromise = (type, p) => (req, res) => {
+    // console.log(`got ${type} request`);
+
+    const bs = [];
+    req.on('data', d => {
+      bs.push(d);
+    });
+    req.once('end', () => {
+      const d = Buffer.concat(bs);
+      p.accept(d);
+      res.end();
+    });
+    req.once('error', p.reject);
+  };
+  const volumePromise = makePromise();
+  app.put('/volume.glb', _readIntoPromise('glb', volumePromise));
+  app.use(express.static(__dirname));
+  const server = http.createServer(app);
+  const connections = [];
+  server.on('connection', c => {
+    connections.push(c);
+  });
+  server.listen(port, () => {
+    open(`https://xrpackage.org/volume.html?srcWbn=http://localhost:${port}/a.wbn&dstVolume=http://localhost:${port}/volume.glb`);
+  });
+
+  const [volumeUint8Array] = await Promise.all([volumePromise]);
+  server.close();
+  for (let i = 0; i < connections.length; i++) {
+    connections[i].destroy();
+  }
+
+  const bundleBuffer = fs.readFileSync(output);
+  const bundle = new wbn.Bundle(bundleBuffer);
+
+  const res = bundle.getResponse('https://xrpackage.org/manifest.json');
+  const s = res.body.toString('utf8');
+  const manifestJson = JSON.parse(s);
+  const {start_url: startUrl} = manifestJson;
+
+  const primaryUrl = 'https://xrpackage.org';
+  const builder = new wbn.BundleBuilder(primaryUrl + '/' + startUrl);
+  for (const u of bundle.urls) {
+    const {pathname} = new url.URL(u);
+    if (pathname !== '/manifest.json') {
+      const res = bundle.getResponse(u);
+      const type = res.headers['content-type'];
+      const data = res.body;
+      builder.addExchange(primaryUrl + pathname, 200, {
+        'Content-Type': type,
+      }, data);
+    }
+  }
+
+  if (volumeUint8Array.length > 0) {
+    let volumeIcon = manifestJson.icons && manifestJson.icons.find(icon => icon.type === 'model/gltf-binary+preview');
+    if (!volumeIcon) {
+      builder.addExchange(primaryUrl + '/xrpackage_volume.glb', 200, {
+        'Content-Type': 'model/gltf-binary+preview',
+      }, volumeUint8Array);
+
+      volumeIcon = {
+        src: 'xrpackage_volume.glb',
+        'type': 'model/gltf-binary+preview',
+      };
+      if (!Array.isArray(manifestJson.icons)) {
+        manifestJson.icons = [];
+      }
+      manifestJson.icons.push(volumeIcon);
+    }
+  }
+
+  builder.addExchange(primaryUrl + '/manifest.json', 200, {
+    'Content-Type': 'application/json',
+  }, JSON.stringify(manifestJson, null, 2));
+
+  const buffer = builder.createBundle();
+  fs.writeFileSync(output, buffer);
+};
 
 let handled = false;
 yargs
@@ -981,6 +1071,20 @@ yargs
     }
 
     await _screenshotApp(argv.input);
+  })
+  .command('volume [input]', 'generate a volume of the package at [input]', yargs => {
+    yargs
+      .positional('input', {
+        describe: 'built package to volume (a.wbn)',
+      });
+  }, async argv => {
+    handled = true;
+
+    if (typeof argv.input !== 'string') {
+      argv.input = 'a.wbn';
+    }
+
+    await _volumeApp(argv.input);
   })
   .command('view [input]', 'view contents of input .wbn file', yargs => {
     yargs
