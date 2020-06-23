7ecc5ad4bcf046a037b0ecd713d556d07d7579e4
Shubham Jain <shubham@sjain.dev>
2020-06-23 11:44:00 +0100

    Move init command into own file

--- cli.js
+++ cli.js
@@ -460,31 +460,7 @@ yargs
   .command(require('./commands/run'))
   .command(require('./commands/inspect'))
   .command(require('./commands/install'))
-  .command('init', 'initialize xrpackage with manifest.json', yargs => {
-    yargs
-      .positional('input', {
-        describe: 'input file to build',
-        // default: 5000
-      })
-      .positional('output', {
-        describe: 'output file to write',
-        // default: 5000
-      });
-  }, async argv => {
-    handled = true;
-
-    if (fs.existsSync('manifest.json')) {
-      console.warn('manifest.json already exists; doing nothing');
-    } else {
-      fs.writeFileSync('manifest.json', JSON.stringify({
-        name: 'My WebXR App',
-        description: 'Describe your WebXR application',
-        xr_type: 'webxr-site@0.0.1',
-        start_url: 'index.html',
-      }, null, 2));
-      console.log('manifest.json');
-    }
-  })
+  .command(require('./commands/init'))
   .command('build [input] [output]', 'build xrpackage .wbn from [input] and write to [output]', yargs => {
     yargs
       .positional('input', {
---
+++ commands/init.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+
+module.exports = {
+  command: 'init',
+  describe: 'initialize xrpackage with manifest.json',
+  builder: yargs => {
+    yargs
+      .positional('input', {
+        describe: 'input file to build',
+      })
+      .positional('output', {
+        describe: 'output file to write',
+      });
+  },
+  handler: async () => {
+    if (fs.existsSync('manifest.json')) {
+      console.warn('manifest.json already exists; doing nothing');
+    } else {
+      fs.writeFileSync('manifest.json', JSON.stringify({
+        name: 'My WebXR App',
+        description: 'Describe your WebXR application',
+        xr_type: 'webxr-site@0.0.1',
+        start_url: 'index.html',
+      }, null, 2));
+      console.log('manifest.json');
+    }
+  },
+};
