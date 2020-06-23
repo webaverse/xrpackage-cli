0c6800a8458de708ac59adc47b43d30a33d19959
Shubham Jain <shubham@sjain.dev>
2020-06-23 13:50:17 +0100

    Further cleanup for build.js
    - Moves _readdirRecursive to be a helper function outside of the handler
    - Shorten logic for xrtype detection

--- commands/build.js
+++ commands/build.js
@@ -10,6 +10,46 @@ const {primaryUrl} = require('../constants');
 
 const _removeUrlTail = u => u.replace(/(?:\?|#).*$/, '');
 
+const xrTypes = [
+  {regex: /\.gltf$/, xrType: 'gltf@0.0.1', description: 'GLTF JSON model'},
+  {regex: /\.glb$/, xrType: 'gltf@0.0.1', description: 'GLTF binary model'},
+  {regex: /\.vrm$/, xrType: 'vrm@0.0.1', description: 'VRM model'},
+  {regex: /\.vox$/, xrType: 'vox@0.0.1', description: 'VOX model'},
+  {regex: /\.html$/, xrType: 'webxr-site@0.0.1', description: 'WebXR app'},
+];
+
+const xrTypeToMimeType = {
+  'gltf@0.0.1': 'model/gltf+json',
+  'vrm@0.0.1': 'application/octet-stream',
+  'vox@0.0.1': 'application/octet-stream',
+  'webxr-site@0.0.1': 'text/html',
+};
+
+const _readdirRecursive = (rootDirectory, output) => {
+  const result = [];
+  const paths = ignoreWalk.sync({
+    path: rootDirectory, // root dir to start in. defaults to process.cwd()
+    ignoreFiles: ['.gitignore'], // list of filenames. defaults to ['.ignore']
+    includeEmpty: false, // true to include empty dirs, default false
+    follow: false, // true to follow symlink dirs, default false
+  });
+  const cwd = process.cwd();
+  const outputPathname = path.resolve(cwd, output);
+  for (const pathname of paths) {
+    const fullPathname = path.resolve(cwd, rootDirectory, pathname);
+    if (!/^\.git\//.test(pathname) && fullPathname !== outputPathname) {
+      const stats = fs.lstatSync(fullPathname);
+      if (stats.isFile()) {
+        result.push({
+          fullPathname: fullPathname,
+          pathname: '/' + pathname,
+        });
+      }
+    }
+  }
+  return result;
+};
+
 module.exports = {
   command: 'build [input] [output]',
   describe: 'build xrpackage .wbn from [input] and write to [output]',
@@ -36,70 +76,26 @@ module.exports = {
     }
 
     let fileInput, startUrl, xrType, xrDetails, mimeType, name, description, directory;
-    const xrTypeToMimeType = {
-      'gltf@0.0.1': 'model/gltf+json',
-      'vrm@0.0.1': 'application/octet-stream',
-      'vox@0.0.1': 'application/octet-stream',
-      'webxr-site@0.0.1': 'text/html',
-    };
     const _detectType = input => {
-      if (/\.gltf$/.test(input)) {
+      const type = xrTypes.find(type => type.regex.test(input));
+      if (type) {
         fileInput = input;
-        xrType = 'gltf@0.0.1';
+        xrType = type.xrType;
         xrDetails = {};
         startUrl = path.basename(fileInput);
         mimeType = xrTypeToMimeType[xrType];
         name = path.basename(input);
-        description = 'GLTF JSON model';
-        directory = null;
-      } else if (/\.glb$/.test(input)) {
-        fileInput = input;
-        xrType = 'gltf@0.0.1';
-        xrDetails = {};
-        startUrl = path.basename(fileInput);
-        mimeType = xrTypeToMimeType[xrType];
-        name = path.basename(input);
-        description = 'GLTF binary model';
-        directory = null;
-      } else if (/\.vrm$/.test(input)) {
-        fileInput = input;
-        xrType = 'vrm@0.0.1';
-        xrDetails = {};
-        startUrl = path.basename(fileInput);
-        mimeType = xrTypeToMimeType[xrType];
-        name = path.basename(input);
-        description = 'VRM model';
-        directory = null;
-      } else if (/\.vox$/.test(input)) {
-        fileInput = input;
-        xrType = 'vox@0.0.1';
-        xrDetails = {};
-        startUrl = path.basename(fileInput);
-        mimeType = xrTypeToMimeType[xrType];
-        name = path.basename(input);
-        description = 'VOX model';
-        directory = null;
-      } else if (/\.html$/.test(input)) {
-        fileInput = input;
-        xrType = 'webxr-site@0.0.1';
-        xrDetails = {};
-        startUrl = path.basename(fileInput);
-        mimeType = xrTypeToMimeType[xrType];
-        name = path.basename(input);
-        description = 'WebXR app';
+        description = type.description;
         directory = null;
       } else if (/\.json$/.test(input)) {
         const s = (() => {
           try {
             return fs.readFileSync(input);
           } catch (err) {
-            if (err.code === 'ENOENT') {
-              return null;
-            } else {
-              return null;
-            }
+            return null;
           }
         })();
+
         if (s) {
           let error;
           const j = (() => {
@@ -110,6 +106,7 @@ module.exports = {
               return null;
             }
           })();
+
           if (j) {
             const hasXrType = typeof j.xr_type === 'string';
             const hasStartUrl = typeof j.start_url === 'string';
@@ -143,85 +140,63 @@ module.exports = {
       }
     };
     _detectType(path.resolve(process.cwd(), argv.input));
-    if (fileInput) {
-      const fileData = fs.readFileSync(fileInput);
-      // console.log('got data', data.length);
 
-      const files = [
-        {
-          url: '/' + startUrl,
-          type: mimeType,
-          data: fileData,
-        },
-        {
-          url: '/manifest.json',
-          type: 'application/json',
-          data: JSON.stringify({
-            name,
-            description,
-            xr_type: xrType,
-            start_url: startUrl,
-            xr_details: xrDetails,
-          }, null, 2),
-        },
-      ];
-      if (directory) {
-        const _readdirRecursive = rootDirectory => {
-          const result = [];
-          const paths = ignoreWalk.sync({
-            path: rootDirectory, // root dir to start in. defaults to process.cwd()
-            ignoreFiles: ['.gitignore'], // list of filenames. defaults to ['.ignore']
-            includeEmpty: false, // true to include empty dirs, default false
-            follow: false, // true to follow symlink dirs, default false
+    if (!fileInput) return;
+
+    const fileData = fs.readFileSync(fileInput);
+    // console.log('got data', data.length);
+
+    const files = [
+      {
+        url: '/' + startUrl,
+        type: mimeType,
+        data: fileData,
+      },
+      {
+        url: '/manifest.json',
+        type: 'application/json',
+        data: JSON.stringify({
+          name,
+          description,
+          xr_type: xrType,
+          start_url: startUrl,
+          xr_details: xrDetails,
+        }, null, 2),
+      },
+    ];
+
+    if (directory) {
+      const filenames = _readdirRecursive(directory, argv.output);
+      for (let i = 0; i < filenames.length; i++) {
+        const {fullPathname, pathname} = filenames[i];
+        if (!files.some(({url}) => url === pathname)) {
+          const type = mime.getType(fullPathname) || 'application/octet-stream';
+          const data = fs.readFileSync(fullPathname);
+          files.push({
+            url: pathname,
+            type,
+            data,
           });
-          const cwd = process.cwd();
-          const outputPathname = path.resolve(cwd, argv.output);
-          for (const pathname of paths) {
-            const fullPathname = path.resolve(cwd, rootDirectory, pathname);
-            if (!/^\.git\//.test(pathname) && fullPathname !== outputPathname) {
-              const stats = fs.lstatSync(fullPathname);
-              if (stats.isFile()) {
-                result.push({
-                  fullPathname: fullPathname,
-                  pathname: '/' + pathname,
-                });
-              }
-            }
-          }
-          return result;
-        };
-        const filenames = _readdirRecursive(directory);
-        for (let i = 0; i < filenames.length; i++) {
-          const {fullPathname, pathname} = filenames[i];
-          if (!files.some(({url}) => url === pathname)) {
-            const type = mime.getType(fullPathname) || 'application/octet-stream';
-            const data = fs.readFileSync(fullPathname);
-            files.push({
-              url: pathname,
-              type,
-              data,
-            });
-          }
         }
       }
-
-      const builder = new wbn.BundleBuilder(primaryUrl + '/' + startUrl);
-      for (let i = 0; i < files.length; i++) {
-        const file = files[i];
-        const {url, type, data} = file;
-        builder.addExchange(primaryUrl + url, 200, {
-          'Content-Type': type,
-        }, data);
-      }
-      const uint8Array = builder.createBundle();
-      // console.log('got bundle', uint8Array.byteLength);
-
-      fs.writeFileSync(argv.output, uint8Array);
-
-      if (argv.screenshot) {
-        await screenshotApp(argv.output);
-      }
-      console.log(argv.output);
     }
+
+    const builder = new wbn.BundleBuilder(primaryUrl + '/' + startUrl);
+    for (let i = 0; i < files.length; i++) {
+      const file = files[i];
+      const {url, type, data} = file;
+      builder.addExchange(primaryUrl + url, 200, {
+        'Content-Type': type,
+      }, data);
+    }
+    const uint8Array = builder.createBundle();
+    // console.log('got bundle', uint8Array.byteLength);
+
+    fs.writeFileSync(argv.output, uint8Array);
+
+    if (argv.screenshot) {
+      await screenshotApp(argv.output);
+    }
+    console.log(argv.output);
   },
 };
