af12d8cf43be1b9a1436422af4622804a7a6bd8a
Shubham Jain <shubham@sjain.dev>
2020-06-23 12:23:16 +0100

    Use CommonJS module for constants

--- constants.js
+++ constants.js
@@ -1,8 +1,19 @@
-export const apiHost = 'https://ipfs.exokit.org/ipfs';
-export const packagesEndpoint = 'https://packages.exokit.org';
-export const tokenHost = 'https://tokens.webaverse.com';
-export const network = 'rinkeby';
-export const infuraApiKey = '4fb939301ec543a0969f3019d74f80c2';
-export const rpcUrl = `https://${network}.infura.io/v3/${infuraApiKey}`;
-export const port = 9999;
-export const primaryUrl = 'https://xrpackage.org';
+const apiHost = 'https://ipfs.exokit.org/ipfs';
+const packagesEndpoint = 'https://packages.exokit.org';
+const tokenHost = 'https://tokens.webaverse.com';
+const network = 'rinkeby';
+const infuraApiKey = '4fb939301ec543a0969f3019d74f80c2';
+const rpcUrl = `https://${network}.infura.io/v3/${infuraApiKey}`;
+const port = 9999;
+const primaryUrl = 'https://xrpackage.org';
+
+module.exports = {
+  apiHost,
+  packagesEndpoint,
+  tokenHost,
+  network,
+  infuraApiKey,
+  rpcUrl,
+  port,
+  primaryUrl,
+};
