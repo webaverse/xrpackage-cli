e1ac8e52cff16cc7ec28953b7606ff79fda8120e
Avaer Kazmer <a@modules.io>
2020-06-29 17:32:24 -0400

    Add initial contracts url

--- constants.js
+++ constants.js
@@ -4,6 +4,7 @@ const tokenHost = 'https://tokens.webaverse.com';
 const network = 'rinkeby';
 const infuraApiKey = '4fb939301ec543a0969f3019d74f80c2';
 const rpcUrl = `https://${network}.infura.io/v3/${infuraApiKey}`;
+const contractsUrl = `http://contracts.exokit.org:3001`;
 const port = 9999;
 const primaryUrl = 'https://xrpackage.org';
 
@@ -14,6 +15,7 @@ module.exports = {
   network,
   infuraApiKey,
   rpcUrl,
+  contractsUrl,
   port,
   primaryUrl,
 };
